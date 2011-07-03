e0d54fe1f2c32d129f124e0d23907b9c9395996d
Andrew Serputko <aserputko@gmail.com>
2011-07-03 14:43:26 +0200

    Created ajax query

--- off-line-web-application/js/offlinewebapp/models/offLineWebAppPresantationModel.js
+++ off-line-web-application/js/offlinewebapp/models/offLineWebAppPresantationModel.js
@@ -4,7 +4,7 @@
 				
 		initialize : function (options) {
 			Model.prototype.initialize.call(this, options);
-			this.url = '/off-line-web-application/data.php'
+			this.url = 'http://stp.org.ua/tojson/auth'
 			var result = this.save();
 			console.log(result)
 		}	
